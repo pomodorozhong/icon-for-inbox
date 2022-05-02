82801dc11a9a8802fca0f4129d0c7c236e399de2
pomodorozhong <pomodorozhong@gmail.com>
2022-05-03 07:01:16 +0800

    build📦(package.swift): setup as tutorial said

--- Package.swift
+++ Package.swift
@@ -4,25 +4,34 @@
 import PackageDescription
 
 let package = Package(
-  name: "IconForInbox",
+  name: "icon-for-inbox",
+  platforms: [
+    .macOS(.v11)
+  ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "IconForInbox",
       targets: ["IconForInbox"]),
+    .executable(
+      name: "export",
+      targets: ["Export"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/darrarski/swiftui-app-icon-creator.git", from: "1.2.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "IconForInbox",
-      dependencies: []),
-    .testTarget(
-      name: "IconForInboxTests",
+      dependencies: [
+        .product(name: "AppIconCreator", package: "swiftui-app-icon-creator")
+      ]),
+    .executableTarget(
+      name: "Export",
       dependencies: ["IconForInbox"]),
   ]
 )
